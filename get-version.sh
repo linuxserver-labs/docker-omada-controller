8ba5983673e102b28a823dc0b9624d08655292ec
TheSpad <git@spad.co.uk>
2021-12-22 16:46:22 +0000

    Account for major versions breaking release page

--- get-version.sh
+++ get-version.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-APP_VERSION=$(curl -sL https://www.tp-link.com/uk/support/download/omada-software-controller/v4/ \
+APP_VERSION=$(curl -sL https://www.tp-link.com/uk/support/download/omada-software-controller/ \
     | grep -E -m 1 -io 'https?://[^ ]+.tar.gz' \
     | awk -F "/" '{print $NF}' \
     | awk -F '_' '{print $4}');
-  
+
+if [[ -z "$APP_VERSION" ]]; then
+    PREVIOUS_MAJOR_VER=$(curl -sL "https://www.tp-link.com/uk/support/download/omada-software-controller" | grep -E -io 'https://www.tp-link.com/uk/support/download/omada-software-controller/\S{2}' | awk -F '/' '{print $8}' | awk 'NR==2{print $1}')
+
+    APP_VERSION=$(curl -sL "https://www.tp-link.com/uk/support/download/omada-software-controller/${PREVIOUS_MAJOR_VER}" \
+    | grep -E -m 1 -io 'https?://[^ ]+.tar.gz' \
+    | awk -F "/" '{print $NF}' \
+    | awk -F '_' '{print $4}');
+fi
+
 printf "%s" "${APP_VERSION}"
