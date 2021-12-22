0e58120ac2a4964b0d7e1e80a741255550cff733
TheSpad <git@spad.co.uk>
2021-12-22 15:00:40 +0000

    Fix yet another breaking change to their release pages

--- get-version.sh
+++ get-version.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-APP_VERSION=$(curl -sL https://www.tp-link.com/uk/support/download/omada-software-controller/ \
+APP_VERSION=$(curl -sL https://www.tp-link.com/uk/support/download/omada-software-controller/v4/ \
     | grep -E -m 1 -io 'https?://[^ ]+.tar.gz' \
     | awk -F "/" '{print $NF}' \
     | awk -F '_' '{print $4}');
